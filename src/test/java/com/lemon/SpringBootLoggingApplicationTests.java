ccf8409c202c8756738d6a383f021cbd897a4eca
lemonliu1993 <2305247091@qq.com>
2023-07-30 13:27:52 +0800

    SpringBoot 默认日志

---
+++ src/test/java/com/lemon/SpringBootLoggingApplicationTests.java
@@ -0,0 +1,33 @@
+package com.lemon;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * Created by lemoon on 2023/7/30 13:20
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootLoggingApplicationTests {
+
+    //记录器
+    Logger logger = LoggerFactory.getLogger(getClass());
+
+    @Test
+    public void contextLoads(){
+//        System.out.println();
+
+        //日志的级别：
+        //由低到高 trace<debug<info<warn<error
+        //可以调整输出的日志级别；日志就只会在这个级别及以后的高级别生效
+        logger.trace("这是trace日志...");
+        logger.debug("这是debug日志...");
+        logger.info("这是info日志...");
+        logger.warn("这是warn日志...");
+        logger.error("这是error日志...");
+    }
+}
