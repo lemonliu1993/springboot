f9d34e219dc63726749c3d4aaf5925414fcf462c
lemonliu1993 <2305247091@qq.com>
2023-08-09 22:56:47 +0800

    thymeleaf 语法

--- src/main/java/com/lemon/controller/ThymeleafController.java
+++ src/main/java/com/lemon/controller/ThymeleafController.java
@@ -3,6 +3,9 @@ package com.lemon.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Arrays;
+import java.util.Map;
+
 /**
  * Created by lemoon on 2023/8/8 17:44
  */
@@ -10,9 +13,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class ThymeleafController {
 
 
-    @RequestMapping("/success")
-    public String success() {
+    @RequestMapping("/test")
+    public String test() {
         //classpath:/templates/success.html
         return "test";
+    }
+
+    //查出用户数据，在页面展示
+    @RequestMapping("/success")
+    public String success(Map<String,Object> map) {
+        map.put("hello","<h1>你好</h1>");
+        map.put("users", Arrays.asList("zhangsan","lisi","wangwu"));
+        //classpath:/templates/success.html
+        return "success";
     }
 }
