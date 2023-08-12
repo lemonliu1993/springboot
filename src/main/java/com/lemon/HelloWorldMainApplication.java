c0b26aad0ffc424298533a8d947b93671ebe239a
lemonliu1993 <2305247091@qq.com>
2023-08-12 22:56:56 +0800

    SpringMVC自动配置原理

--- src/main/java/com/lemon/HelloWorldMainApplication.java
+++ src/main/java/com/lemon/HelloWorldMainApplication.java
@@ -2,7 +2,12 @@ package com.lemon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.web.servlet.View;
+import org.springframework.web.servlet.ViewResolver;
+
+import java.util.Locale;
 
 /**
  * Created by lemoon on 2023/7/7 23:33
@@ -14,5 +19,18 @@ public class HelloWorldMainApplication {
 
         //Spring应用启动起来
         SpringApplication.run(HelloWorldMainApplication.class, args);
+    }
+
+
+    @Bean
+    public ViewResolver myViewResolver(){
+        return new MyViewResolver();
+    }
+
+    private static class MyViewResolver implements ViewResolver{
+        @Override
+        public View resolveViewName(String s, Locale locale) throws Exception {
+            return null;
+        }
     }
 }
